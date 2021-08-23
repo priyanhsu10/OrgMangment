c95aa6af8872c143a08d2caf4a22e0feffb233ae
WebSamurai <priyanshuparate@yahoo.in>
2021-08-23 18:49:12 +0530

    added test

--- src/test/java/education/io/educationapi/ApplicationTests.java
+++ src/test/java/education/io/educationapi/ApplicationTests.java
@@ -1,13 +1,22 @@
 package education.io.educationapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+	}
+   @Test
+	public  void  test1()
+   {
+	   Assertions.assertEquals(1,1);
+	   Assertions.assertTrue(true);
+
+   }
 }
---
+++ src/test/java/education/io/educationapi/domain/services/org/BranchDomainServiceTest.java
@@ -0,0 +1,24 @@
+package education.io.educationapi.domain.services.org;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BranchDomainServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+}
